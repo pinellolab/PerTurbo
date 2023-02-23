e04f3bb2a36697e81fbf11ea5a4e4158045e4adf
Logan Blaine <logan.j.blaine@gmail.com>
2023-02-23 11:05:53 -0500

    add skeleton pyro model

---
+++ src/perturbvi/_model.py
@@ -0,0 +1,101 @@
+"""
+A pyro model
+"""
+import logging
+from typing import List, Optional, Union
+
+import numpy as np
+import torch
+from mudata import MuData
+from scvi.dataloaders import DataSplitter
+from scvi.model.base import BaseModelClass
+from scvi.train import PyroTrainingPlan, TrainRunner
+
+from ._module import PerturbVIPyroModule
+
+logger = logging.getLogger(__name__)
+
+class PerturbVIPyroModel(BaseModelClass):
+    def __init__(
+        self,
+        mdata: MuData,
+        **model_kwargs,
+    ):
+        super(PerturbVIPyroModel, self).__init__(mdata)
+
+        # self.summary_stats provides information about dimensions and other tensor info
+
+        self.module = PerturbVIPyroModule(
+            **model_kwargs,
+        )
+
+        self._model_summary_string = (
+            f"MyPyroModel Model with params:\n{self.summary_stats}"
+        )
+
+        # necessary line to get params that will be used for saving/loading
+        self.init_params_ = self._get_init_params(locals())
+
+        logger.info("The model has been initialized")
+
+    def train(
+        self,
+        max_epochs: Optional[int] = None,
+        use_gpu: Optional[Union[str, int, bool]] = None,
+        train_size: float = 1.,
+        validation_size: Optional[float] = None,
+        batch_size: int = 128,
+        plan_kwargs: Optional[dict] = None,
+        **trainer_kwargs,
+    ):
+        """
+        Train the model.
+
+        Parameters
+        ----------
+        max_epochs
+            Number of passes through the dataset. If `None`, defaults to
+            `np.min([round((20000 / n_cells) * 400), 400])`
+        use_gpu
+            Use default GPU if available (if None or True), or index of GPU to use (if int),
+            or name of GPU (if str), or use CPU (if False).
+        train_size
+            Size of training set in the range [0.0, 1.0].
+        validation_size
+            Size of the test set. If `None`, defaults to 1 - `train_size`. If
+            `train_size + validation_size < 1`, the remaining cells belong to a test set.
+        batch_size
+            Minibatch size to use during training.
+        plan_kwargs
+            Keyword args for :class:`~scvi.lightning.TrainingPlan`. Keyword arguments passed to
+            `train()` will overwrite values present in `plan_kwargs`, when appropriate.
+        **trainer_kwargs
+            Other keyword args for :class:`~scvi.lightning.Trainer`.
+        """
+        if max_epochs is None:
+            n_cells = self.adata.n_obs
+            max_epochs = np.min([round((20000 / n_cells) * 400), 400])
+
+        plan_kwargs = plan_kwargs if isinstance(plan_kwargs, dict) else {}
+
+        data_splitter = DataSplitter(
+            self.adata_manager,
+            train_size=train_size,
+            validation_size=validation_size,
+            batch_size=batch_size,
+            use_gpu=use_gpu,
+        )
+        training_plan = PyroTrainingPlan(self.module, **plan_kwargs)
+        runner = TrainRunner(
+            self,
+            training_plan=training_plan,
+            data_splitter=data_splitter,
+            max_epochs=max_epochs,
+            use_gpu=use_gpu,
+            **trainer_kwargs,
+        )
+        return runner()
+
+    @classmethod
+    def setup_mudata():
+        pass
