9dadf7ac02905aa98e5d377b0161679f086fe8d8
Logan Blaine <logan.j.blaine@gmail.com>
2023-02-24 18:39:22 -0500

    module that can learn a per-gene NB model

--- src/perturbvi/_module.py
+++ src/perturbvi/_module.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import pyro
 import pyro.distributions as dist
 import torch
@@ -13,13 +11,31 @@ class PerturbVIPyroModule(PyroBaseModuleClass):
 
     @staticmethod
     def _get_fn_args_from_batch(tensor_dict):
-        x = tensor_dict
-        return x
+        return (tensor_dict[REGISTRY_KEYS.X_KEY],), {}
 
-    @property
+    def create_plates(self, x):
+        n_cells, n_vars = x.shape
+        cell_plate = pyro.plate("cells", n_cells, dim=-2)
+        vars_plate = pyro.plate("vars", n_vars, dim=-1)
+        return (cell_plate, vars_plate)
+
     def model(self, x):
-        pass
+        cell_plate, vars_plate = self.create_plates(x)
+        with vars_plate:
+            log_var_mean = pyro.sample("log_var_mean", dist.Normal(2, 4))
+            log_var_dispersion = pyro.sample("log_var_dispersion", dist.Normal(2, 1))
+            with cell_plate:
+                pyro.sample(
+                    "obs",
+                    dist.NegativeBinomial(total_count=log_var_dispersion.exp(), logits=log_var_mean),
+                    obs=x,
+                )
 
-    @property
     def guide(self, x):
-        pass
+        _, vars_plate = self.create_plates(x)
+        _, n_vars = x.shape
+        log_var_mean_mu = pyro.param("log_var_mean.mu", lambda: torch.full((n_vars,), 0.))
+        log_var_disp_mu = pyro.param("log_var_disp.mu", lambda: torch.full((n_vars,), 0.))
+        with vars_plate:
+            pyro.sample("log_var_mean", dist.Delta(log_var_mean_mu))
+            pyro.sample("log_var_dispersion", dist.Delta(log_var_disp_mu))
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,19 +6,27 @@ from mudata import AnnData, MuData
 import perturbvi
 
 
+rna_key = "rna"
+perturb_key = "grna"
+
+
+@pytest.fixture
+def mdata():
+    n_cells = 20
+    n_genes = 10
+    n_grna = 5
+
+    rna_adata = AnnData(np.random.poisson(size=(n_cells, n_genes)), dtype=np.float64)
+    perturb_adata = AnnData(np.random.binomial(1, 0.5, size=(n_cells, n_grna)), dtype=np.float64)
+    perturb_adata.var_names = "guide" + perturb_adata.var_names
+    return MuData({rna_key: rna_adata, perturb_key: perturb_adata})
+
+
 def test_package_has_version():
     logging.info("version: " + perturbvi.__version__)
 
 
-def test_model_init():
-    rna_key = "rna"
-    perturb_key = "grna"
-
-    rna_adata = AnnData(np.random.poisson(size=(20, 10)), dtype=np.float64)
-    perturb_adata = AnnData(np.random.binomial(1, 0.5, size=(20, 5)), dtype=np.float64)
-    perturb_adata.var_names = 'guide' + perturb_adata.var_names
-    mdata = MuData({rna_key: rna_adata, perturb_key: perturb_adata})
-
+def test_model_init(mdata):
     perturbvi.PERTURBVI.setup_mudata(
         mdata,
         modalities={
@@ -27,7 +35,11 @@ def test_model_init():
         },
     )
     model = perturbvi.PERTURBVI(mdata)
-    print(model.summary_stats)
+    assert model.summary_stats.n_cells == len(mdata)
+    assert model.summary_stats.n_vars == len(mdata[rna_key].var)
+    assert model.summary_stats.n_perturbations == len(mdata[perturb_key].var)
+
+    model.train(max_epochs=2)
 
 
 @pytest.mark.skip(reason="This decorator should be removed when test passes.")
