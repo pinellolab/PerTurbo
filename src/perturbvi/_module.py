ca9f369c5267522cd566e65956c7452c6576cb97
Logan Blaine <logan.j.blaine@gmail.com>
2023-02-27 12:40:11 -0500

    implement mean-field(ish) normal guide

--- src/perturbvi/_module.py
+++ src/perturbvi/_module.py
@@ -20,30 +20,73 @@ class PerturbVIPyroModule(PyroBaseModuleClass):
     def create_plates(self, idx, **tensor_dict):
         return (
             pyro.plate("cells", self.n_cells, dim=-2, subsample=idx),
-            pyro.plate("perturbations", self.n_perturbations, dim=-3),
+            pyro.plate("perturbations", self.n_perturbations, dim=-2),
             pyro.plate("vars", self.n_vars, dim=-1),
         )
 
     def model(self, idx, **tensor_dict):
-        cell_plate, _, var_plate = self.create_plates(idx)
-
+        pyro.module("perturbvi", self)
+        cell_plate, perturbation_plate, var_plate = self.create_plates(idx)
+        library_size = tensor_dict[REGISTRY_KEYS.OBSERVED_LIB_SIZE]
+        perturbations = tensor_dict[REGISTRY_KEYS.PERTURBATION_KEY]
         with var_plate:
-            log_var_mean = pyro.sample("log_var_mean", dist.Normal(0, 4))
-            log_var_dispersion = pyro.sample("log_var_dispersion", dist.Normal(2, 1))
+            with perturbation_plate:
+                perturb_mean_lfc = pyro.sample("perturb_mean_lfc", dist.Cauchy(0.0, 0.1))
+                perturb_disp_lfc = pyro.sample("perturb_disp_lfc", dist.Cauchy(0.0, 0.1))
+            log_var_mean = pyro.sample("log_var_mean", dist.Normal(0.0, 4.0))
+            log_var_dispersion = pyro.sample("log_var_dispersion", dist.Normal(2.0, 1.0))
+
+            nb_log_dispersion = log_var_dispersion.exp() + perturbations @ perturb_disp_lfc
+            nb_log_mean = log_var_mean + perturbations @ perturb_mean_lfc + library_size.log()
+
             with cell_plate:
-                pyro.sample(
+                return pyro.sample(
                     "obs",
                     dist.NegativeBinomial(
                         total_count=log_var_dispersion.exp(),
-                        logits=log_var_mean + tensor_dict[REGISTRY_KEYS.OBSERVED_LIB_SIZE].log(),
+                        logits=nb_log_mean - nb_log_dispersion,
                     ),
                     obs=tensor_dict[REGISTRY_KEYS.X_KEY],
                 )
 
-    def guide(self, idx, **tensor_dict):
-        _, _, var_plate = self.create_plates(idx)
-        log_var_mean_mu = pyro.param("log_var_mean.mu", lambda: torch.full((self.n_vars,), 0.0))
-        log_var_disp_mu = pyro.param("log_var_disp.mu", lambda: torch.full((self.n_vars,), 0.0))
+    def guide(self, idx, init_scale=0.1, **tensor_dict):
+        pyro.module("perturbvi", self)
+        _, perturbation_plate, var_plate = self.create_plates(idx)
+        log_var_mean_mu = pyro.param("log_var_mean.mu", lambda: torch.zeros((self.n_vars,)))
+        log_var_disp_mu = pyro.param("log_var_disp.mu", lambda: torch.zeros((self.n_vars,)))
+        log_var_mean_sigma = pyro.param(
+            "log_var_mean.sigma", lambda: torch.full((self.n_vars,), init_scale), constraint=dist.constraints.positive
+        )
+        log_var_disp_sigma = pyro.param(
+            "log_var_disp.sigma", lambda: torch.full((self.n_vars,), init_scale), constraint=dist.constraints.positive
+        )
+
+        perturb_mean_lfc_mu = pyro.param(
+            "perturb_mean_lfc.mu", lambda: torch.zeros((self.n_perturbations, self.n_vars))
+        )
+        perturb_disp_lfc_mu = pyro.param(
+            "perturb_disp_lfc.mu", lambda: torch.zeros((self.n_perturbations, self.n_vars))
+        )
+        perturb_lfc_mu = torch.stack((perturb_mean_lfc_mu, perturb_disp_lfc_mu), dim=-1)
+
+        perturb_lfc_scale_tril = pyro.param(
+            "perturb_mean_lfc.scale_tril",
+            lambda: torch.eye(2).repeat((self.n_perturbations, self.n_vars, 1, 1)),
+            constraint=dist.constraints.corr_cholesky_constraint,
+        )
+
         with var_plate:
-            pyro.sample("log_var_mean", dist.Delta(log_var_mean_mu))
-            pyro.sample("log_var_dispersion", dist.Delta(log_var_disp_mu))
+            pyro.sample("log_var_mean", dist.Normal(log_var_mean_mu, log_var_mean_sigma))
+            pyro.sample("log_var_dispersion", dist.Normal(log_var_disp_mu, log_var_disp_sigma))
+
+            with perturbation_plate:
+                perturb_lfc = pyro.sample(
+                    "perturb_lfc",
+                    dist.MultivariateNormal(
+                        perturb_lfc_mu,
+                        scale_tril=perturb_lfc_scale_tril,
+                    ),
+                    infer={"is_auxiliary": True},
+                )
+                pyro.sample("perturb_mean_lfc", dist.Delta(perturb_lfc[..., 0]))
+                pyro.sample("perturb_disp_lfc", dist.Delta(perturb_lfc[..., 1]))
