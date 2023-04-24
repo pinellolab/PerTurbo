62527f9f9f5177d0df29bfe59742a9561a933df7
Logan Blaine <logan.j.blaine@gmail.com>
2023-04-24 10:18:04 -0400

    fix AnnData dtype argument in test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -25,8 +25,10 @@ def adata():
             "batch_id": np.random.choice(["batch_1", "batch_2"], size=(n_cells)),
         }
     )
-    rna_counts = np.random.negative_binomial(100, 0.9, size=(n_cells, n_genes))
-    rna_adata = AnnData(rna_counts, obs=total_rna, dtype=np.float64)
+    rna_counts = np.random.negative_binomial(100, 0.9, size=(n_cells, n_genes)).astype(
+        np.float64
+    )
+    rna_adata = AnnData(rna_counts, obs=total_rna)
 
     # generate fake guide status
     rna_adata.obsm[perturb_key] = np.random.binomial(1, 0.5, size=(n_cells, n_grna))
@@ -45,7 +47,7 @@ def mdata(adata: AnnData):
 
     # generate fake guide status
     perturb_adata = AnnData(
-        np.random.binomial(1, 0.5, size=(n_cells, n_grna)), dtype=np.float64
+        np.random.binomial(1, 0.5, size=(n_cells, n_grna)).astype(np.float64)
     )
     perturb_adata.var_names = "guide" + perturb_adata.var_names
 
@@ -61,7 +63,7 @@ def test_package_has_version():
 
 def test_model_mdata(mdata: MuData):
     """Check that we can register our MuData object with our model and perform training"""
-    perturbo.perturbo.setup_mudata(
+    perturbo.PERTURBO.setup_mudata(
         mdata,
         # size_factor_key="lib_size",
         # batch_key="batch_id",
@@ -87,7 +89,7 @@ def test_model_mdata(mdata: MuData):
 
 def test_model_adata(adata: AnnData):
     """Check that we can register our AnnData object with our model and perform training"""
-    perturbo.perturbo.setup_anndata(
+    perturbo.PERTURBO.setup_anndata(
         adata,
         perturb_key,
         categorical_covariates_keys=["batch_id"],
