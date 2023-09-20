cd1d7dd341733d86a65559098226c42bac24862e
Logan Blaine <logan.j.blaine@gmail.com>
2023-09-20 19:55:32 -0400

    don't fit guide effects if no elements specified

--- src/perturbo/_module.py
+++ src/perturbo/_module.py
@@ -76,7 +76,9 @@ class PerTurboPyroModule(PyroBaseModuleClass):
         self.local_effects = gene_by_element is not None
         if self.local_effects:
             if gene_by_element.shape[1] != self.n_elements:
-                raise ValueError("Number of inferred elements does not match gene_by_element matrix shape")
+                raise ValueError(
+                    "Number of inferred elements does not match gene_by_element matrix shape"
+                )
 
             self.register_buffer(
                 "element_by_gene_idx", gene_by_element.T.to_sparse_coo().indices()
@@ -96,8 +98,8 @@ class PerTurboPyroModule(PyroBaseModuleClass):
         self.register_buffer("covariate_prior_sigma", torch.tensor(3.0))
         # self.register_buffer("logit_efficacy_mu", torch.tensor(2.0))
         # self.register_buffer("logit_efficacy_sigma", torch.tensor(1.0))
-        self.register_buffer("logit_efficacy_alpha", torch.tensor(2.0))
-        self.register_buffer("logit_efficacy_beta", torch.tensor(5.0))
+        self.register_buffer("logit_efficacy_alpha", torch.tensor(5.0))
+        self.register_buffer("logit_efficacy_beta", torch.tensor(2.0))
 
         if self.n_factors is not None:
             self.register_buffer("factor_element_prior_scale", torch.tensor(0.01))
@@ -171,25 +173,21 @@ class PerTurboPyroModule(PyroBaseModuleClass):
                 )
 
         # estimate a single efficacy value per guide
-        # alternative: estimate efficacy for each guide--gene *cis* pair
-        with guide_plate:
-            # with guide_effects_plate:
-            # base_dist = dist.Normal(
-            #     self.logit_efficacy_mu, self.logit_efficacy_sigma
-            # )
-            # transforms = [dist.transforms.SigmoidTransform()]
-            # logit_normal = dist.TransformedDistribution(base_dist, transforms)
-            # guide_efficacy_values = pyro.sample("guide_efficacy", logit_normal)
-            guide_efficacy_values = pyro.sample(
-                "guide_efficacy",
-                dist.Beta(self.logit_efficacy_alpha, self.logit_efficacy_beta),
-            )
+        if self.multi_guide:
+            with guide_plate:
+                guide_efficacy_values = pyro.sample(
+                    "guide_efficacy",
+                    dist.Beta(self.logit_efficacy_alpha, self.logit_efficacy_beta),
+                )
             guide_efficacy = guide_efficacy_values * self.guide_by_element
+            ## alternative: estimate efficacy for each guide--gene *cis* pair
             # guide_efficacy = torch.sparse_coo_tensor(
             #     self.guide_by_element_idx,
             #     guide_efficacy_values,
             #     size=(self.n_perturbations, self.n_elements),
             # )
+        else:
+            guide_efficacy = self.guide_by_element
 
         if self.n_factors is not None:
             with element_plate, factor_plate_1:
