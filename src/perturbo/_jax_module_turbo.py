daf8aa2292c6f101819dbf5ffd610f59ecb23dc1
Logan Blaine <logan.j.blaine@gmail.com>
2024-03-23 19:21:41 -0400

    implement jax factor model

--- src/perturbo/_jax_module_turbo.py
+++ src/perturbo/_jax_module_turbo.py
@@ -3,7 +3,7 @@ from typing import Literal, Optional
 import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
-from numpyro.infer.autoguide import AutoNormal, init_to_median
+from numpyro.infer.autoguide import AutoDelta, AutoGuideList, AutoNormal, init_to_median
 from tensorflow_probability.substrates.jax import distributions as tfd
 
 
@@ -14,7 +14,7 @@ def _create_plates(
     n_cells=None,
     n_guides=None,
     subsample_size=None,
-    guide_targets=None,
+    guide_target_elements=None,
     n_genes=None,
     **kwargs,
 ):
@@ -28,18 +28,29 @@ def _create_plates(
         n_covariates = 1
     else:
         n_covariates = covariates.shape[1]
-    if guide_targets is None:
+    if guide_target_elements is None:
         n_elements = n_guides
     else:
-        n_elements = guide_targets.shape[1]
-
+        n_elements = guide_target_elements.shape[1]
+    n_factors = 1000
     cell_plate = numpyro.plate("cells", n_cells, dim=-2, subsample_size=subsample_size)
     covariate_plate = numpyro.plate("covariates", n_covariates, dim=-2)
     gene_plate = numpyro.plate("genes", n_genes, dim=-1)
     guide_plate_T = numpyro.plate("guides_T", n_guides, dim=-2)
     element_plate = numpyro.plate("elements", n_elements, dim=-2)
     guide_plate = numpyro.plate("guides", n_guides, dim=-1)
-    plates = (cell_plate, covariate_plate, guide_plate, guide_plate_T, element_plate, gene_plate)
+    factor_plate = numpyro.plate("factors", n_factors, dim=-1)
+    loading_plate = numpyro.plate("factors_T", n_factors, dim=-2)
+    plates = (
+        cell_plate,
+        covariate_plate,
+        guide_plate,
+        guide_plate_T,
+        element_plate,
+        gene_plate,
+        factor_plate,
+        loading_plate,
+    )
     return plates
 
 
@@ -47,7 +58,8 @@ def perturbseq_model_turbo(
     genes: Optional[jnp.ndarray] = None,
     guide_obs: Optional[jnp.ndarray] = None,
     covariates: Optional[jnp.ndarray] = None,
-    guide_targets: Optional[jnp.ndarray] = None,
+    guide_target_elements: Optional[jnp.ndarray] = None,
+    element_target_genes: Optional[jnp.ndarray] = None,
     log2_fc: Optional[jnp.ndarray] = None,
     gene_mean: Optional[jnp.ndarray] = None,
     gene_disp: Optional[jnp.ndarray] = None,
@@ -72,39 +84,55 @@ def perturbseq_model_turbo(
         assert genes.shape[0] == guide_obs.shape[0]
     if covariates is None:
         covariates = jnp.zeros((n_cells, 1))
-    if guide_targets is None:
-        guide_targets = jnp.eye(n_guides)
+    if guide_target_elements is None:
+        guide_target_elements = jnp.eye(n_guides)
 
     # create plates
-    cell_plate, covariates_plate, guide_plate, guide_plate_T, element_plate, gene_plate = _create_plates(
-        genes,
-        covariates=covariates,
-        guide_targets=guide_targets,
-        n_cells=n_cells,
-        n_guides=n_guides,
-        n_genes=n_genes,
-        subsample_size=subsample_size,
+    cell_plate, covariates_plate, guide_plate, guide_plate_T, element_plate, gene_plate, factor_plate, loading_plate = (
+        _create_plates(
+            genes,
+            covariates=covariates,
+            guide_target_elements=guide_target_elements,
+            n_cells=n_cells,
+            n_guides=n_guides,
+            n_genes=n_genes,
+            subsample_size=subsample_size,
+        )
     )
 
     # sample gene-level params
     with gene_plate:
         baseline_mean = numpyro.sample("mean", dist.LogNormal(0.0, 4.0), obs=gene_mean)
-        dispersion = numpyro.sample("dispersion", dist.LogNormal(2.0, 2.0), obs=gene_disp)
+        dispersion = numpyro.sample("dispersion", dist.LogNormal(0.0, 3.0), obs=gene_disp)
 
     # sample guide efficiency
-    with guide_plate_T, gene_plate:
+    # with guide_plate_T, gene_plate:
+    with guide_plate_T:
         guide_efficiency = numpyro.sample("efficiency", dist.Beta(efficiency_alpha, efficiency_beta), obs=efficiency)
-
-    # sample element effect sizes
-    with element_plate, gene_plate:
+        guide_efficiency = 1
+        # # sample element effect sizes (dense full)
+        # with element_plate, gene_plate:
         inclusion_probs = jnp.stack([1.0 - prior_inclusion_prob, prior_inclusion_prob], axis=-1)
         effect_scales = jnp.stack([non_effect_scale, effect_scale], axis=-1)
         mix_dist = dist.Categorical(inclusion_probs)
         effect_dist = dist.Normal(0.0, effect_scales)
-        log2_fc = numpyro.sample("log2_fold_change", dist.MixtureSameFamily(mix_dist, effect_dist), obs=log2_fc)
+        spike_and_slab_dist = dist.MixtureSameFamily(mix_dist, effect_dist)
+        #     log2_fc = numpyro.sample("log2_fold_change", dist.MixtureSameFamily(mix_dist, effect_dist), obs=log2_fc)
+
+    # sample cis element effect sizes (sparse)
+    with element_plate:
+        log2_fc = numpyro.sample("log2_fold_change", dist.Normal(0, effect_scale), obs=log2_fc) * element_target_genes
+
+    with element_plate, factor_plate:
+        factors = numpyro.sample("factor", dist.Cauchy(0, 0.01))
+
+    with loading_plate, gene_plate:
+        loadings = numpyro.sample("loading", dist.Cauchy(0, 0.01))
+    # log2_fc = numpyro.deterministic("log2_fold_change", factors @ loadings)
+    log2_fc += factors @ loadings
 
     with covariates_plate, gene_plate:
-        covariate_weights = numpyro.sample("covariate_weights", dist.Normal(0.0, 1.0))
+        covariate_weights = numpyro.sample("covariate_weight", dist.Normal(0.0, 1.0))
 
     # sample gene values for each cell
     with cell_plate:
@@ -120,7 +148,7 @@ def perturbseq_model_turbo(
             guide_obs = numpyro.sample("guide_obs", dist.Binomial(1, probs=guide_prob), obs=guide_obs)
 
         # (n_cells x n_guide) @ (n_guide x n_element @ n_element x n_gene) = n_cells x n_genes
-        guide_effect = guide_obs @ (guide_targets @ log2_fc * guide_efficiency) * jnp.log(2)
+        guide_effect = guide_obs @ (guide_target_elements @ log2_fc * guide_efficiency) * jnp.log(2)
         guide_effect += covariate_effect
 
         if likelihood == "Poisson":
@@ -142,8 +170,29 @@ def perturbseq_model_turbo(
 
 
 def perturbseq_guide_autonormal_turbo(init_loc_fn=init_to_median):
-    return AutoNormal(
+    guide = AutoNormal(
         perturbseq_model_turbo,
         init_loc_fn=init_loc_fn,
         create_plates=_create_plates,
     )
+    return guide
+    guide = AutoGuideList(
+        perturbseq_model_turbo,
+        init_loc_fn=init_loc_fn,
+        create_plates=_create_plates,
+    )
+    guide.append(
+        AutoNormal(
+            numpyro.handlers.block(
+                numpyro.handlers.seed(perturbseq_model_turbo, rng_seed=0), hide=["factor", "loading"]
+            )
+        )
+    )
+    guide.append(
+        AutoDelta(
+            numpyro.handlers.block(
+                numpyro.handlers.seed(perturbseq_model_turbo, rng_seed=1), expose=["factor", "loading"]
+            )
+        )
+    )
+    return guide
