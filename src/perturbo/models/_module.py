506c1be7b10e5718dabb05281158bcaf9689563d
Logan Blaine <logan.j.blaine@gmail.com>
2025-04-10 07:24:39 -0400

    remove redundant term

--- src/perturbo/models/_module.py
+++ src/perturbo/models/_module.py
@@ -355,7 +355,7 @@ class PerTurboPyroModule(PyroBaseModuleClass):
         # Account for cell-specific latent "perturbation status" variable(s)
         with cell_plate:
             if self.efficiency_mode == "scaled":
-                perturbed = guide_efficacy.T * guides_observed
+                perturbed = guide_efficacy.T
             elif self.efficiency_mode == "mixture":
                 pert_prob = guides_observed @ guide_efficacy
                 perturbed = pyro.sample("perturbed", dist.Bernoulli(pert_prob), infer={"enumerate": "parallel"})
